e55ad26273a20efb89b1f437f8c40311952c1a8d
Jeremy Eberle <jwe4ec@virginia.edu>
2025-08-29 15:55:58 -0500

    Test semtree WIP

--- code/00.3_test_group_w_semtree.R
+++ code/00.3_test_group_w_semtree.R
@@ -10,8 +10,11 @@
 # Before running script, restart R (CTRL+SHIFT+F10 on Windows) and set working 
 # directory to parent folder
 
-# Resource: https://brandmaier.github.io/semtree/
+# Resource (source of example code below): https://brandmaier.github.io/semtree/
 # - Note: Articles tab above seems more up to date than "browseVignettes("semtree")"
+# - See papers at https://brandmaier.github.io/semtree/index.html
+# - For plot of implied mixture model from another simulated data example, see
+# https://brandmaier.github.io/semtree/articles/score-based-tests.html
 
 # ---------------------------------------------------------------------------- #
 # Store working directory, check correct R version, load packages ----
@@ -64,6 +67,8 @@ names(growth.data) <- c(paste0("X", 1:M), "P1")
 
 # Specify a linear latent growth curve model using OpenMx’s path specification. The model has 
 # five observed variables. Residual variances are assumed to be identical over time.
+
+# Note: binary P1 covariate is not part of the model
 
 manifests <- names(growth.data)[1:5]
 
@@ -133,7 +138,7 @@ growthCurveModel <- mxRun(growthCurveModel)
 
 # Grow a SEM tree using the semtree function, which takes the model and the dataset as input. 
 # If not specified otherwise, SEM tree will assume that all variables in the dataset which 
-# are not observed variables are potential predictors.
+# are not observed variables [included in the SEM] are potential predictors
 
 # Per R documentation, this creates a tree that recursively partitions a dataset such that the 
 # partitions maximally differ with respect to the model-predicted distributions. Each resulting 
@@ -144,18 +149,21 @@ growthCurveModel <- mxRun(growthCurveModel)
 # another over all predictors included in the dataset (see "semtree.control()" defaults)
 
 tree_naive <- semtree(model = growthCurveModel, data = growth.data,
-                      predictors = NULL) # TODO: By default, all variables that are in dataset and
-                                         # not part of model are potential predictors (doesn't this
-                                         # contradict what the tutorial says above?)
+                      predictors = NULL) # By default, all variables that are in dataset and
+                                         # not part of model are potential predictors
 
 # Examine tree
 
 summary(tree_naive)
 plot(tree_naive)
 tree_naive$control
-tree_naive # Has many other elements
 
-# TODO: What is it using as predictors? It seems to have split on "P1", but that was observed
+tree_naive$model # Single-group SEM fit to full sample (N = 1000)
+round(tree_naive$lr, 2) == 1683.14 # LR comparing single-group SEM to multigroup SEM (split per binary P1 covariate)
+tree_naive$p            == 0       # p value for LR test
+
+tree_naive$left_child$model  # SEM fit to left child subgroup (where P1 == 1; N = 526)
+tree_naive$right_child$model # SEM fit to right child subgroup (where P1 == 0; N = 474)
 
 # ---------------------------------------------------------------------------- #
 # Run a tree using "score" selection method ----
@@ -165,7 +173,8 @@ tree_naive # Has many other elements
 
 # Score-based tests for variable and split-point selection are preferable because 
 # they are fast to compute, perform unbiased variable selection, and have better 
-# statistical power than some other selection algorithms proposed earlier
+# statistical power than some other selection algorithms proposed earlier. See
+# Arnold et al. (2021; https://doi.org/10.3389/fpsyg.2020.564403)
 
 # Use "score" selection method and Bonferroni-correction to adjust for multiple 
 # testing of predictors
@@ -178,31 +187,19 @@ tree_score <- semtree(model = growthCurveModel, data = growth.data,
 # Examine tree
 
 summary(tree_score)
-plot(tree_score) # Yielded the same tree, but with different "LR" and "df" values
+plot(tree_score)
 tree_score$control
-tree_score # Has many other elements
 
-# TODO: Where is the significance test for the difference between the subgroups?
+tree_score$model # Single-group SEM fit to full sample (N = 1000)
+round(tree_score$lr, 2) == 818.54 # LR comparing single-group SEM to multigroup SEM (split per binary P1 covariate)
+tree_score$p.uncorrected # 1.52e-173 (p value before Bonferroni correction)
+tree_score$p            == 0      # p value for LR test
 
+tree_score$left_child$model  # SEM fit to left child subgroup (where P1 == 1; N = 526)
+tree_score$right_child$model # SEM fit to right child subgroup (where P1 == 0; N = 474)
 
-
-
-# TODO: For plot of implied mixture model from another simulated data example, see
-# https://brandmaier.github.io/semtree/articles/score-based-tests.html
-
-
-
-
-# TODO: Find and read paper on method to understand further
-# - See papers at https://brandmaier.github.io/semtree/index.html
-
-
-
-
-# TODO: Seems like we'd fit the networks in OpenMx though (not use raw data), using 
-# OpenMx or lavaan syntax. Might be useful if we need to group participants based on 
-# their saturated models (vs. constrained models in S-GIMME)
-
-
-
-
+# Note: We'd have to specify the template SEM in OpenMx using either OpenMx or lavaan 
+# syntax, and that SEM would be fit in all potential subgroups (allowing the parameter
+# estimates to differ in each subgroup) split based on covariates not in the networks 
+# (e.g., demographics). By contrast, S-GIMME identifies subgroups without covariates
+# and conducts subgroup-specific model searches.
